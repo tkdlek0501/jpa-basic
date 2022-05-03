d6cdba5d08a6ca7806b4d25c707c081ec073aef0
HenJune <73812257+tkdlek0501@users.noreply.github.com>
2022-05-03 15:02:59 +0900

    프로젝트 세팅

---
+++ src/test/java/jpa/basic/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.basic.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
